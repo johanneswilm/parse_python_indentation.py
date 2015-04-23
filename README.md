d391fcf2e9cbb0ed77e97e700e32c37b768ce4f7
Johannes Wilm <mail@johanneswilm.org>
2015-04-23 10:45:52 +0200

    make pypi package

--- README.md
+++
@@ -1,2 +0,0 @@
-# parse_python_indention.py
-python function to parse the indention of a python-style indented file.
--- example.py
+++
@@ -1,8 +0,0 @@
-#!/usr/bin/env python
-
-from parse_python_indentation import parse_python_indentation
-
-with open ("test.data", "r") as input_file:
-    rawdata = input_file.read()
-
-print parse_python_indentation(rawdata)
